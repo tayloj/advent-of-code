eeea3c3de83e4e361b30216191e8a9a06187fbe0
Joshua Taylor <joshuaaaron@gmail.com>
2022-12-09 19:42:29 -0500

    Refactor day 1

--- src/test/java/com/northernfugue/aoc2022/Advent1.java
+++ src/test/java/com/northernfugue/aoc2022/Advent1.java
@@ -6,75 +6,33 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.IntStream;
 
 public class Advent1 {
 
   @Test(timeout = 10_000)
-  public void part1Example() throws URISyntaxException, IOException {
-    Elf e1 = part1("day1/sample-input.txt").findFirst().get();
-    assertEquals(4, e1.n);
-    assertEquals(24000, e1.total());
+  public void part1Example() throws Exception {
+    assertEquals(24000, caloriesDescending(Util.input("day1/sample-input.txt")).findFirst().getAsInt());
 
-    assertEquals(66616, part1("day1/input.txt").findFirst().get().total());
-
-
-    assertEquals(199172, part1("day1/input.txt").limit(3).mapToInt(Elf::total).sum());
-
+    assertEquals(66616, caloriesDescending(Util.input("day1/input.txt")).findFirst().getAsInt());
+    assertEquals(199172, caloriesDescending(Util.input("day1/input.txt")).limit(3).sum());
   }
 
-  class Elf {
-
-    private final int n;
-
-    Elf(int n) {
-      this.n = n;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("Elf(%s, %s, %s)", n, total(), cals);
-    }
-
-    List<Integer> cals = new ArrayList<>();
-
-    int total() {
-      return cals.stream().mapToInt(x -> x).sum();
-    }
-  }
-
-  private Stream<Elf> part1(String path) throws IOException {
-
-    List<Elf> elves = new ArrayList<>();
-
-
-    Path p;
-    try {
-      p = Util.input(path);
-    } catch (Exception e) {
-      throw new IOException(e);
-    }
-
-    int n = 0;
-    try (BufferedReader rr = Files.newBufferedReader(p)) {
-      String line = rr.readLine();
-      while (null != (line = rr.readLine())) {
-        Elf e = new Elf(++n);
-        elves.add(e);
-        do {
-          e.cals.add(Integer.parseInt(line));
-          line = rr.readLine();
-        } while (line != null && !line.isEmpty());
+  private IntStream caloriesDescending(Path p) throws IOException {
+    Deque<int[]> cals = new ArrayDeque<>();
+    try (BufferedReader reader = Files.newBufferedReader(p)) {
+      for (String line = ""; line != null; line = reader.readLine()) {
+        if (line.isEmpty()) {
+          cals.push(new int[] {0});
+        } else {
+          cals.peek()[0] += Integer.parseInt(line);
+        }
       }
     }
-
-    return elves.stream().sorted(Comparator.comparing(Elf::total).reversed());
+    return cals.stream().mapToInt(x -> -x[0]).sorted().map(x -> -x);
   }
-
 }
